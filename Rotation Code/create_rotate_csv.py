009120004deda8e08fbc311455ed936958b58152
andrewharrisonGH <andy.har0020@gmail.com>
2025-06-05 17:37:42 +1000

    Change to create separate csv files for easier multiprocessing

--- Rotation Code/create_rotate_csv.py
+++ Rotation Code/create_rotate_csv.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
-data = []
+
+i=1
 for x_rot in range(0,360,5):
+    data = []
     for y_rot in range(0,360,5):
             data.append({'x_rot': str(x_rot), 'y_rot': str(y_rot), 'z_rot': '0'})
 
-df = pd.DataFrame(data)
+    df = pd.DataFrame(data)
 
-df.to_csv('rotations.csv', index=False)
+    df.to_csv('./Rotations/rotations'+str(i)+'.csv', index=False)
 
-print("CSV file 'rotations.csv' written successfully.")
+    print('CSV file "./Rotations/rotations'+str(i)+'.csv" written successfully.')
+    i+=1
